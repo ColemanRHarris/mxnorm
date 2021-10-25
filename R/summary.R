5b29f2f63981071bdba3e71ec5c8c13513acb1aa
Cole <coleman.r.harris@vanderbilt.edu>
2021-10-25 10:56:52 -0500

    setup generic S3 summary method

---
+++ R/summary.R
@@ -0,0 +1,19 @@
+#' Setup S3 method for `summary`
+#'
+#' @param x used to define S3 method (later assumed to be `mx_dataset` object)
+#'
+#' @return NULL
+summary <- function(x) UseMethod("summary")
+
+#' Extension of `summary` S3 method to summarize `mx_dataset` objects
+#'
+#' @param x `mx_dataset` object to summarize
+#'
+#' @return TBD
+#' @export
+#'
+#' @examples summary(mxnorm::mx_sample)
+summary.mx_dataset <- function(x){
+    length(x)
+}
+
