a0058eb721626a3cfc64166b670cc5bed596358e
Cole <coleman.r.harris@vanderbilt.edu>
2021-11-12 14:39:12 -0600

    script to add global variables, pass CRAN notes

---
+++ R/global.R
@@ -0,0 +1,14 @@
+## add variables to global environment
+utils::globalVariables(c("U1",
+                         "U2",
+                         "actual_slide",
+                         "ad_p_value",
+                         "ad_test_statistic",
+                         "cluster_slide",
+                         "marker",
+                         "marker_threshold",
+                         "misclass_error",
+                         "slide_threshold",
+                         "std_ad_test_statistic",
+                         "level",
+                         "sd"))
